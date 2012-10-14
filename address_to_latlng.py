413434a0829b45272a35c5a3a78b1f3642f7fef0
urey <urey001@gmail.com>
2012-10-14 11:31:27 +0800

    gmaps packages added
    do something with gmaps

---
+++ address_to_latlng.py
@@ -0,0 +1,19 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        google maps address to latitute & longitute
+# Purpose:
+#
+# Author:      urey
+#
+# Created:     14/10/2012
+# Copyright:   (c) urey 2012
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+from googlemaps import GoogleMaps
+
+def main():
+    address = 'beihang university'
+    print GoogleMaps().address_to_latlng(address)
+    pass
+
+if __name__ == '__main__':
+    main()
